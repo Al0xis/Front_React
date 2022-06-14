28f55ad552458902cc5f572f661da56b531c8be4
MarcoAntonioAPK <antonio.apk.dev@gmail.com>
2022-06-14 03:31:32 -0500

    Primer commit - Marco

--- client/src/pages/Inicio.js
+++ client/src/pages/Inicio.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 function Inicio() {
   return (
     <div className='inicio'>
-        <h1> Inicio </h1>
+
+      <div class="containter">
+        <div class="row align-items-center">
+          <div class="col">
+            <div class="card w-75">
+              <div class="card-body text-center">
+                <p class="card-text ">Inicio de sesión</p>
+                <select class="fs-3" name="usuarios">
+                  <option value="1">Usuario 1</option>
+                  <option value="2">Usuario 2</option>
+                  <option value="3">Usuario 3</option>
+                  <option value="4">Usuario 4</option>
+                </select>
+                <br/>
+                <a href="#" class="btn btn-primary active" role="button" data-bs-toogle="button" aria-pressed="true">Acceder</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>  
+
     </div>
   )
 }
